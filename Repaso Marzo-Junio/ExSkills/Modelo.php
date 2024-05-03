3df8afe0c4a6d8c51337dcb0879e7a59baa0f3f7
AlvarocsED <acastellanos02@educarex.es>
2024-05-03 11:20:37 +0200

    2

--- Repaso Marzo-Junio/ExSkills/Modelo.php
+++ Repaso Marzo-Junio/ExSkills/Modelo.php
@@ -14,6 +14,34 @@ class Modelo{
             echo $e->getMessage();
         }
     }
+    function obtenerModalidad(){
+        $resultado=array();
+        try{
+            $consulta = $this->conexion->query('SELECT * from modalidad');
+            if($consulta->execute()){
+                while($fila=$consulta->fetch()){
+                    $resultado[]=new Modalidad($fila['id'],$fila['modalidad']);                   
+                }
+            }
+        }catch(PDOException $e){
+            echo $e->getMessage();
+        }
+        return $resultado;
+    }
+    function obtenerAlumnoModalidad($modalidadElegida){
+        $resultado=array();
+        try{
+            $consulta = $this->conexion->query('SELECT * from alumno where modalidad = (select id from modalidad where modalidad ='$modalidadElegida);
+            if($consulta->execute()){
+                while($fila=$consulta->fetch()){
+                    $resultado[]=new Alumno($fila['nombre   ']);                   
+                }
+            }
+        }catch(PDOException $e){
+            echo $e->getMessage();
+        }
+        return $resultado;
+    }
 /**
      * Get the value of conexion
      */
--- Repaso Marzo-Junio/ExSkills/skills.php
+++ Repaso Marzo-Junio/ExSkills/skills.php
@@ -1,3 +1,19 @@
+<?php
+require_once 'Modelo.php';
+
+$bd = new Modelo();
+if($bd->getConexion()==null){
+	$mensaje = 'Error, no hay conexión con la bd';
+}
+session_start();
+if (isset($_POST['selModalidad'])) {
+    $modalidadSeleccionada = $_POST['modalidad'];
+    $_SESSION['modalidad'] = $modalidadSeleccionada;
+    if (isset($_SESSION['modalidad'])) {
+        $modalidadSeleccionada = $_SESSION['modalidad'];
+        bd->obtenerAlumnosModalidad();  
+}
+?>  
 <!DOCTYPE html>
 <html lang="en">
 
@@ -11,12 +27,20 @@
     <div>
         <h1 style='color:red;'>Mensajes</h1>
     </div>
+    <?php if (!isset($_SESSION['alumno']) && isset($_SESSION['modalidad'])) : ?>
+        <!-- Sección de Alumno: solo visible si no hay alumno seleccionado y hay modalidad seleccionada -->
     <form action="skills.php" method="post">
         <div>
             <h1 style='color:blue;'>Modalidad</h1>
             <label for="tienda">Modalidad</label><br />
 
             <select name="modalidad">
+            <?php
+			$modalidad = $bd->obtenerModalidad();
+			foreach($modalidades as $m){
+			echo '<option value="'.$m->getId().'">'.$m->getModalidad().'</option>';	
+			}
+			?>
 
             </select>
             <button type="submit" name="selModalidad">Seleccionar Modalidad</button>
@@ -25,7 +49,12 @@
             <h1 style='color:blue;'>Alumno</h1>
             <label for="tienda">Alumno</label><br />
             <select name="alumno">
-
+            <?php
+			$alumnomodalidad = $bd->obtenerAlumnoModalidad();
+			foreach($alumno as $a){
+			echo '<option value="'.$a->getId().'">'.$a->getNombre().'</option>';	
+			}
+			?>
             </select>
             <button type="submit" name="selAlumno">Seleccionar Alumno</button>
         </div>
@@ -68,6 +97,10 @@
             <button type="submit" name="finalizar">Finalizar Corrección</button>
         </div>
     </form>
+    <?php if (isset($_SESSION['alumno'])) : ?>
+        <!-- Sección de Corrección y calificaciones: solo visible si hay un alumno seleccionado -->
+        <div>
+            <h1 style='color:blue;'>Corrección y calificaciones</h1>
+        </div>
 </body>
-
 </html>
